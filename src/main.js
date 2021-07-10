487f9441920711129f3508619ceff6551b449a24
hefengen <hefengen@hotmail.com>
2021-07-10 22:58:31 +0800

    完成管理员端页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
 Vue.config.productionTip = false
 
+import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
+
+import './views/admin/assets/css/icon.css';
+
+Vue.use(ElementUI, {
+  size: 'small'
+});
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
