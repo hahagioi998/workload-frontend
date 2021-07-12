115a056e38fb10dcc87c4f3064404bf0e190da1f
hefengen <hefengen@hotmail.com>
2021-07-12 09:07:11 +0800

    完成管理员端的页面设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,89 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/admin/page/Home.vue'
+import Admin from '../views/admin/pages/Admin.vue'
+import Home from '../views/workload/pages/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/admin',
-    name: 'Home',
-    component: Home,
-    children: [
-      {
-        path: "/tabs",
-        name: "tabs",
-        meta: {
-          title: 'tab标签'
-        },
-        component: () => import (
-            /* webpackChunkName: "tabs" */
-            "../views/admin/page/Tabs.vue")
-      }
-    ]
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/admin/',
+        name: 'Admin',
+        component: Admin,
+        children: [
+            // 标签页
+            {
+                path: "/tabs",
+                name: "tabs",
+                meta: {
+                    title: 'tab标签'
+                },
+                component: () => import (
+                    /* webpackChunkName: "tabs" */
+                    "../views/admin/pages/Tabs.vue")
+            }, {
+                path: "/project-list",
+                name: "ProjectList",
+                meta: {
+                    title: '项目管理'
+                },
+                component: () => import ("../views/admin/pages/ProjectList")
+            }, {
+                path: "/new-project",
+                name: "NewProject",
+                meta: {
+                    title: '新建项目'
+                },
+                component: () => import ("../views/admin/pages/NewProject")
+            }, {
+                path: "/project-workload-list",
+                name: "ProjectWorkloadList",
+                meta: {
+                    title: '项目工时列表'
+                },
+                component: () => import ("../views/admin/pages/ProjectWorkloadList")
+            }, {
+                path: "/personal-workload-list",
+                name: "PersonalWorkloadList",
+                meta: {
+                    title: '个人工时统计'
+                },
+                component: () => import ("../views/admin/pages/PersonalWorkloadList")
+            }, {
+                path: "/employee-list",
+                name: "EmployeeList",
+                meta: {
+                    title: '员工管理'
+                },
+                component: () => import ("../views/admin/pages/EmployeeList")
+            }, {
+                path: "/new-employee",
+                name: "NewEmployee",
+                meta: {
+                    title: '新建项目'
+                },
+                component: () => import ("../views/admin/pages/NewEmployee")
+            }
+
+        ]
+    },
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
--- src/views/admin/components/Sidebar.vue
+++ src/views/admin/components/Sidebar.vue
@@ -51,127 +51,64 @@
 </template>
 
 <script>
-// import bus from "../common/bus";
 export default {
     name: "vSidebar",
     data() {
         return {
             items: [
                 {
-                    icon: "el-icon-lx-home",
+                    icon: "el-icon-guide",
                     index: "dashboard",
                     title: "系统首页",
+                },
+                {
+                    icon: "el-icon-guide",
+                    index: "project",
+                    title: "项目管理",
                     subs: [
                         {
-                            index: "form",
-                            title: "基本表单"
+                            // index 是 route 的 path
+                            index: "project-list",
+                            title: "项目列表"
+                        },
+                        {
+                            index: "new-project",
+                            title: "新增项目"
                         },
                     ]
                 },
                 {
-                    icon: "el-icon-lx-home",
-                    index: "article-list",
-                    title: "文章列表"
-                },
-                {
-                    icon: "el-icon-lx-emoji",
-                    index: "article",
-                    title: "文章发布"
-                },
-                {
-                  icon: "el-icon-lx-emoji",
-                  index: "category",
-                  title: "文章分类管理"
-                },
-                {
-                    icon: "el-icon-lx-cascades",
-                    index: "table",
-                    title: "基础表格"
-                },
-                {
-                    icon: "el-icon-lx-copy",
-                    index: "tabs",
-                    title: "tab选项卡"
-                },
-                {
-                    icon: "el-icon-lx-calendar",
-                    index: "3",
-                    title: "表单相关",
+                    icon: "el-icon-guide",
+                    index: "workload",
+                    title: "工时统计",
                     subs: [
                         {
-                            index: "form",
-                            title: "基本表单"
+                            // index 是 route 的 path
+                            index: "project-workload-list",
+                            title: "项目工时统计"
                         },
-                        //         {
-                        //             index: "3-2",
-                        //             title: "三级菜单",
-                        //             subs: [
-                        //                 {
-                        //                     index: "editor",
-                        //                     title: "富文本编辑器"
-                        //                 },
-                        //                 {
-                        //                     index: "markdown",
-                        //                     title: "markdown编辑器"
-                        //                 }
-                        //             ]
-                        //         },
                         {
-                            index: "upload",
-                            title: "文件上传"
-                        }
+                            index: "personal-workload-list",
+                            title: "个人工时统计"
+                        },
                     ]
                 },
                 {
-                    icon: "el-icon-lx-emoji",
-                    index: "icon",
-                    title: "自定义图标"
-                },
-                {
-                    icon: "el-icon-pie-chart",
-                    index: "charts",
-                    title: "schart图表"
-                },
-                // {
-                //     icon: "el-icon-rank",
-                //     index: "6",
-                //     title: "拖拽组件",
-                //     subs: [
-                //         {
-                //             index: "drag",
-                //             title: "拖拽列表"
-                //         },
-                //         {
-                //             index: "dialog",
-                //             title: "拖拽弹框"
-                //         }
-                //     ]
-                // },
-                {
-                    icon: "el-icon-lx-global",
-                    index: "i18n",
-                    title: "国际化功能"
-                },
-                {
-                    icon: "el-icon-lx-warn",
-                    index: "7",
-                    title: "错误处理",
+                    icon: "el-icon-guide",
+                    index: "employee",
+                    title: "员工管理",
                     subs: [
                         {
-                            index: "permission",
-                            title: "权限测试"
+                            // index 是 route 的 path
+                            index: "employee-list",
+                            title: "员工列表"
                         },
                         {
-                            index: "404",
-                            title: "404页面"
-                        }
+                            index: "new-employee",
+                            title: "新增员工"
+                        },
                     ]
                 },
-                {
-                    icon: "el-icon-lx-redpacket_fill",
-                    index: "/donate",
-                    title: "支持作者"
-                }
             ]
         };
     },
